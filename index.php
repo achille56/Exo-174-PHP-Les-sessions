6cfd33cd4262ea3c4a0a8a50ca0df6e909bf2fdb
Jean-Michel <leclercqjm59@gmail.com>
2023-01-27 10:27:01 +0100

    exo 174

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 // Premiere ligne
 
+session_start();
+$_SESSION["color"] = "rouge";
+
+echo "Je viens de definir la session 'color'.";
+?>
+<a href="index2.php">Index2</a>
 
 
 
---
+++ index2.php
@@ -0,0 +1,4 @@
+<?php
+
+session_start();
+echo "Voici la valeur de la couleur choisis: ". $_SESSION["color"];
